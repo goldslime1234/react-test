3f9c1d055d9956fe89dc7d08cc2aa58229f52acb
goldslime1234 <kenjisniper@gmail.com>
2020-06-13 19:22:56 +0800

    Updates

---
+++ precache-manifest.ceeffc6a21c93cd334540e2ec6635f65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd0bbdbc35542e643575f677ed27a6d",
+    "url": "/goldslime1234/react-test.git/index.html"
+  },
+  {
+    "revision": "6ca41bb2efd72b6fb48a",
+    "url": "/goldslime1234/react-test.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc9242ceec1ee0ccf537",
+    "url": "/goldslime1234/react-test.git/static/js/2.8a711379.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goldslime1234/react-test.git/static/js/2.8a711379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca41bb2efd72b6fb48a",
+    "url": "/goldslime1234/react-test.git/static/js/main.38f09f7e.chunk.js"
+  },
+  {
+    "revision": "5e2497eccdda1c699d3c",
+    "url": "/goldslime1234/react-test.git/static/js/runtime-main.2239fe05.js"
+  }
+]);
